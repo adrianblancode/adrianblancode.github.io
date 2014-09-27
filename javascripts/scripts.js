fd8099b67d5576a06b97ef969e773a871b1262d6
Adrian Blanco <adrianblp@gmail.com>
2014-09-27 18:43:36 +0200

    rework logo on mouseover, refactor code

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,12 +1,25 @@
 //Toggles the visibility of the element specified by id
 function toggle(id){
+
     var elem = document.getElementById(id);
     
-    console.log(elem.style.display);
-
     if(elem.style.display != "block"){
         elem.style.display = "block";
     } else {
         elem.style.display = "none";
+    }
+}
+
+function toggleLogo(color){
+
+    var dl = document.getElementById("dl");
+    var ml = document.getElementById("ml");
+
+    if(color == 'red'){
+        ml.src = "images/logored.svg";
+        dl.src = "images/logored.svg";
+    } else {
+        ml.src = "images/logo.svg";
+        dl.src = "images/logo.svg";
     }
 }
