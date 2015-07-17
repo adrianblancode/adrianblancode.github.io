74451252a95894a8130c2cc2560e6fa4aa5eea35
Adrian Blanco <adrianblp@gmail.com>
2015-07-17 11:56:15 +0200

    Change icon font, add animations

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,6 +1,6 @@
 //Toggles the visibility of the menu
 function toggleMenu(){
-    
+
     var elem = document.getElementById('categories');
     
     if(elem.style.display != "block"){
@@ -22,4 +22,33 @@ function toggleLogo(color){
     } else {
         logo.src = "/images/logo.png";
     }
+}
+
+//Determines which navigation to highlight
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#sidebar a:not(#mobile-nav a)').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 100){
+            currLink.removeClass("active");
+            $("#sidebar a:last").addClass("active");
+        }
+        else if (refElement.selector != "#" && refElement.position().top <= scrollPos + 80 && refElement.position().top + refElement.height() > scrollPos) {
+            $('#categories ul li a').removeClass("active");
+            currLink.addClass("active");
+
+            //We remove the transparency on the meny when scrolled down far enough
+            if(refElement.selector == "#home"){
+              $("#sidebar").removeClass("scrolled");
+
+            } else {
+              $("#sidebar").addClass("scrolled");
+            }
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
 }
