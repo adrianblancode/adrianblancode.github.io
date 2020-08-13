012d4932c752ffa4ff394fb146fbe14adf0ee1eb
adrianblp <adrianblp@gmail.com>
2020-08-13 15:53:34 +0200

    Updates

---
+++ precache-manifest.4dd7d3b56b80e2c37f0c4cdcdb573edb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8617054f60f5088fcf6e23680f764d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f4105323fa30ad5d796",
+    "url": "/static/css/main.b39a62a1.chunk.css"
+  },
+  {
+    "revision": "d36697d7e2eaebbc0b5b",
+    "url": "/static/js/2.61b00ac5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61b00ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4105323fa30ad5d796",
+    "url": "/static/js/main.223fe7c6.chunk.js"
+  },
+  {
+    "revision": "6e8d485bb366edb68751",
+    "url": "/static/js/runtime-main.6e8a501c.js"
+  },
+  {
+    "revision": "a17f44eb64748ae2783c764217d814ea",
+    "url": "/static/media/AvenirLTStd-Heavy.a17f44eb.woff"
+  },
+  {
+    "revision": "f81b2654584a0f82c613f2c4888b0ce3",
+    "url": "/static/media/AvenirLTStd-Heavy.f81b2654.woff2"
+  },
+  {
+    "revision": "d4adbe3577884a47e48c9f1d3c937432",
+    "url": "/static/media/AvenirLTStd-Medium.d4adbe35.woff"
+  },
+  {
+    "revision": "fa02ec9b5e8d892da485cfa80c381a1b",
+    "url": "/static/media/AvenirLTStd-Medium.fa02ec9b.woff2"
+  },
+  {
+    "revision": "25049e09326fa4d45cf2b311f359dbd2",
+    "url": "/static/media/AvenirLTW02-55Roman.25049e09.woff2"
+  },
+  {
+    "revision": "47793e6c6e0aef4f86044c31d9fb7acd",
+    "url": "/static/media/AvenirLTW02-55Roman.47793e6c.woff"
+  },
+  {
+    "revision": "0fbdc00b94017d7ac56eabee7238fea4",
+    "url": "/static/media/AvenirLTW02-95Black.0fbdc00b.woff"
+  },
+  {
+    "revision": "ef86cf16f59651486d1f45a4cf30dd0e",
+    "url": "/static/media/AvenirLTW02-95Black.ef86cf16.woff2"
+  },
+  {
+    "revision": "1c6ad8c5fa0b3e0ba0b4a5457a8e9d31",
+    "url": "/static/media/github.1c6ad8c5.svg"
+  },
+  {
+    "revision": "3f8923b33e009aa104bf5535ce96ed59",
+    "url": "/static/media/gmail.3f8923b3.svg"
+  },
+  {
+    "revision": "7b020cb4c83cf48c6e83cb8a12eb76ce",
+    "url": "/static/media/linkedin.7b020cb4.svg"
+  }
+]);
