900c52f9db843b29e2dc4b22359f0ec7dfd7f0cb
adrianblp <adrianblp@gmail.com>
2020-08-13 15:58:39 +0200

    Updates

---
+++ precache-manifest.3e6ebba8ddd7db0bdcf17f1a6ead2665.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e70c84ad76f6ebeb0df19bbe00a886b",
+    "url": "/adrianblancode.github.io/index.html"
+  },
+  {
+    "revision": "b78708a481dea5c698b4",
+    "url": "/adrianblancode.github.io/static/css/main.7aca691e.chunk.css"
+  },
+  {
+    "revision": "b9b9775ce0853c7f0cef",
+    "url": "/adrianblancode.github.io/static/js/2.9aefe35d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adrianblancode.github.io/static/js/2.9aefe35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78708a481dea5c698b4",
+    "url": "/adrianblancode.github.io/static/js/main.660e7c32.chunk.js"
+  },
+  {
+    "revision": "c51e3928fc5ad4bc5af6",
+    "url": "/adrianblancode.github.io/static/js/runtime-main.4c979ece.js"
+  },
+  {
+    "revision": "a17f44eb64748ae2783c764217d814ea",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTStd-Heavy.a17f44eb.woff"
+  },
+  {
+    "revision": "f81b2654584a0f82c613f2c4888b0ce3",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTStd-Heavy.f81b2654.woff2"
+  },
+  {
+    "revision": "d4adbe3577884a47e48c9f1d3c937432",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTStd-Medium.d4adbe35.woff"
+  },
+  {
+    "revision": "fa02ec9b5e8d892da485cfa80c381a1b",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTStd-Medium.fa02ec9b.woff2"
+  },
+  {
+    "revision": "25049e09326fa4d45cf2b311f359dbd2",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTW02-55Roman.25049e09.woff2"
+  },
+  {
+    "revision": "47793e6c6e0aef4f86044c31d9fb7acd",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTW02-55Roman.47793e6c.woff"
+  },
+  {
+    "revision": "0fbdc00b94017d7ac56eabee7238fea4",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTW02-95Black.0fbdc00b.woff"
+  },
+  {
+    "revision": "ef86cf16f59651486d1f45a4cf30dd0e",
+    "url": "/adrianblancode.github.io/static/media/AvenirLTW02-95Black.ef86cf16.woff2"
+  },
+  {
+    "revision": "1c6ad8c5fa0b3e0ba0b4a5457a8e9d31",
+    "url": "/adrianblancode.github.io/static/media/github.1c6ad8c5.svg"
+  },
+  {
+    "revision": "3f8923b33e009aa104bf5535ce96ed59",
+    "url": "/adrianblancode.github.io/static/media/gmail.3f8923b3.svg"
+  },
+  {
+    "revision": "7b020cb4c83cf48c6e83cb8a12eb76ce",
+    "url": "/adrianblancode.github.io/static/media/linkedin.7b020cb4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dd7d3b56b80e2c37f0c4cdcdb573edb.js"
+  "/adrianblancode.github.io/precache-manifest.3e6ebba8ddd7db0bdcf17f1a6ead2665.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adrianblancode.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
