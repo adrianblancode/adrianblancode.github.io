ad882169dc2a19b90cd531f4437acdeb5f04e57c
adrianblp <adrianblp@gmail.com>
2020-08-16 20:12:29 +0200

    Redesign, add recent projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,46 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect } from 'react';
 import gmail from './vectors/gmail.svg';
 import linkedin from './vectors/linkedin.svg';
 import github from './vectors/github.svg';
+import coop from './images/coop-showcase.png';
+import goahead from './images/goahead-showcase.png';
+import sj from './images/sj-showcase.png';
+import whitelines from './images/whitelines-showcase.jpg';
 import './App.scss';
 
 function App() {
-  useLayoutEffect(() => {
+  useEffect(() => {
 
     let landscape: boolean = window.innerWidth > window.innerHeight
 
     // High zoom looks really busy in vertical
-    let zoom: number = landscape ? 1.0 : 0.3;
+    let zoom: number = landscape ? 1 : 0.4;
 
     (window as any).VANTA.FOG({
-      el: "#root",
+      el: "#vanta",
       minHeight: 200.00,
       minWidth: 200.00,
-      highlightColor: 0x101015,
-      midtoneColor: 0xd02040,
-      lowlightColor: 0x7020b0,
-      baseColor: 0x221122,
+      highlightColor: 0xff2060,
+      midtoneColor: 0xde3163,
+      lowlightColor: 0x772088,
+      baseColor: 0x25004a,
       blurFactor: 0.35,
       zoom: zoom,
-      speed: 0.5
+      speed: 0.6
     })
+  }, [])
 
-  })
   return (
     <div id="app">
-      <section className="hero is-fullheight">
-        <div className="hero-body">
-          <Intro />
-        </div>
-        <section className="hero-footer"> 
-          <Footer />
+      <div id="vanta">
+        <section className="hero is-fullheight">
+          <div className="hero-body">
+            <Intro />
+          </div>
         </section>
-      </section>
+      </div>
+      <Experience />
+      <Footer />
     </div>
   );
 }
@@ -43,10 +48,10 @@ function App() {
 function Intro() {
   return (
     <div className="container">
-      <h1 className="title is-1 is-size-3-touch is-spaced">Hi, I'm Adrian! I build world-class Android apps.</h1>
-      <p className="mb-4">I am a Stockholm based developer currently working freelance, previously Software Engineer and Platform Lead at <a href="https://bontouch.com">Bontouch.</a></p>
-      <p className="mb-4">I have done everything from mobile development, technical leadership, web development, graphics programming, audio analysis, to even co-founding a company creating interactive mixed reality experiences.</p>
-      <p className="mb-4">If this sounds interesting, feel free to <a href="/files/Adrian_Blanco_CV.pdf">check out my CV</a> or just <a href="mailto:adrianblancode@gmail.com">say hello.</a></p>
+      <h1 className="title is-1 is-size-3-touch is-spaced fadein-1">Hi, I'm Adrian! I build world-class Android apps.</h1>
+      <p className="mb-4 fadein-2">I am a Stockholm based developer currently working freelance, previously Software Engineer and Platform Lead at <a href="https://bontouch.com">Bontouch.</a></p>
+      <p className="mb-4 fadein-3">I have worked with everything from mobile development, technical leadership, web development, graphics programming, audio analysis, to even co-founding a company creating interactive mixed reality experiences.</p>
+      <p className="mb-4 fadein-4">If this sounds interesting, feel free to <a href="/files/Adrian_Blanco_CV.pdf">check out my CV</a> or just <a href="mailto:adrianblancode@gmail.com">say hello.</a></p>
       <p></p>
     </div>
   )
@@ -55,35 +60,49 @@ function Intro() {
 
 function Experience() {
   return (
-    <div className="container">
-      <h4 className="title is-4">Recent Projects</h4>
-      <div className="columns">
-        <div className="column is-6-tablet">
-          <img src="/images/coop-showcase.png" alt="Coop Showcase" />
-          <h3 className="subtitle is-5">Coop</h3>
+    <section className="section">
+      <div className="container">
+        <h4 className="title is-5">Recent Projects</h4>
+        <div className="columns is-variable is-4">
+
+          <div className="column is-6-tablet mb-4">
+            <img src={coop} className="portfolio-image" alt="Coop Showcase" />
+            <h3 className="subtitle is-6 mb-2 mt-4">Coop</h3>
+            <p className="is-size-7 mb-1">Coop is a grocery store chain in the Nordics with a strong focus on online shopping.</p>
+            <p className="is-size-7">I led the development of their new app for Android as platform lead, building one of the largest ecommerce grocery apps in the Nordics from the ground up and serving hundreds of thousands of users. As platform lead I ensured high technical quality through technical leadership, architecture, software engineering best practices, code reviews, among others.</p>
+          </div>
+
+          <div className="column is-6-tablet mb-4">
+            <img src={goahead} className="portfolio-image" alt="GoAhead Showcase" />
+            <h3 className="subtitle is-6 mb-2 mt-4">GoAhead</h3>
+            <p className="is-size-7 mb-1">I worked with GoAhead as a developer for the ground up rewrite of their 12 bus apps handling passenger travel and tickets for buses all over the UK.</p>
+            <p className="is-size-7">The work was a close collaboration between the Stockholm and London offices, and during the final months of the project I relocated to the London office to lead the app experience for the Android platform.</p>
+          </div>
         </div>
-        <div className="column is-6-tablet">
-          <img src="/images/goahead-showcase.png" alt="GoAhead Showcase" />
-          <h3>GoAhead</h3>
+        <div className="columns is-variable is-4">
+
+          <div className="column is-6-tablet mb-4">
+            <img src={sj} className="portfolio-image" alt="SJ Showcase" />
+            <h3 className="subtitle is-6 mb-2 mt-4">Swedish Railways (SJ)</h3>
+            <p className="is-size-7 mb-1">For SJ I developed a suite of internal apps from the ground up. They are currently used by SJ staff to coordinate maintenance and repairs of trains all across Sweden.</p>
+          </div>
+
+          <div className="column is-6-tablet mb-4">
+            <img src={whitelines} className="portfolio-image" alt="Whitelines Showcase" />
+            <h3 className="subtitle is-6 mb-2 mt-4">Whitelines</h3>
+            <p className="is-size-7 mb-1">Whitelines Link is an app to scan and organize your notes.</p>
+            <p className="is-size-7">For the Android version, developed a new major release including redesigning the app with a new design language and implementing new features for scanning notes.</p>
+          </div>
         </div>
       </div>
-      <div className="columns">
-        <div className="column is-6-tablet">
-          <img src="/images/sj-showcase.png" alt="SJ Showcase" />
-          <h3>Swedish Railways (SJ)</h3>
-        </div>
-        <div className="column is-6-tablet">
-          <img src="/images/whitelines-showcase.jpg" alt="Whitelines Showcase" />
-          <h3>Whitelines</h3>
-        </div>
-      </div>
-    </div>
+    </section>
   )
 }
 
 function Footer() {
   return (
-      <div className="container pb-4">
+    <section className="section pb-4">
+      <div className="container">
         <div className="level">
           <div className="level-left">
             <div className="level-item">
@@ -105,6 +124,7 @@ function Footer() {
           </div>
         </div>
       </div>
+    </section>
   )
 }
 
